b7033dce07faaa11bc143e90f2ae838406aae712
Constantine <constantine.shibaev@gmail.com>
2023-04-25 23:04:26 +0300

    added and stylized page not-found to the app

--- src/routes/ErrorPage/ErrorPage.tsx
+++ src/routes/ErrorPage/ErrorPage.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Button } from '@mui/material'
 import { HOME } from '../../constants/routes'
-import '../../styles/error-page.scss'
+import Home from '@mui/icons-material/Home'
+
+import './style.scss'
 
 const ErrorPage = () => {
     const navigate = useNavigate()
@@ -12,16 +14,24 @@ const ErrorPage = () => {
     }
 
     return (
-        <div className="error-page">
-            <div className="error-container">
-                <h1>Whoops... something went wrong!</h1>
-                <p className="error-text">Sorry, an unexpected error has occurred.</p>
-                <Button
-                    className="get-back-btn"
-                    onClick={handleGetBackClick}
-                >
-                    Get back to Home
-                </Button>
+        <div className="nf-page">
+            <div className="nf-container">
+                <div className="nf-main">
+                    <h1>404</h1>
+                </div>
+                <div className="nf-content">
+                    <p className="nf-primary-text">Whoops... looks like you're lost.</p>
+                    <p className="nf-secondary-text">The page you are looking for not available!</p>
+                    <Button
+                        color="primary"
+                        variant="contained"
+                        className="get-back-btn"
+                        startIcon={<Home />}
+                        onClick={handleGetBackClick}
+                    >
+                        Get back to Home
+                    </Button>
+                </div>
             </div>
         </div>
     )
